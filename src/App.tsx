ba23655d618af274f43b67af7125b429c035edde
shahidulllah <mdshahidsumon177@gmail.com>
2025-01-04 22:53:21 +0600

    working on redux app

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,10 @@
-import { useState } from 'react'
-
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div>
+        <h1>This is counter App</h1>
       </div>
     </>
   )
